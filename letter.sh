62222b4ac642a70ddf1ba89f84149d1dfc7cce4e
RSWilli <bartel.wilhelm@gmail.com>
2022-03-14 16:10:28 +0100

    watch script

--- letter.sh
+++ letter.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-pandoc "$1" -s -o "$1.pdf" --template="./letter-template.tex"
+function cleanup() {
+    exit 0
+}
+
+trap cleanup EXIT
+
+while true; do
+    echo "compiling $1"
+    pandoc "$1" -s -o "$1.pdf" --template="./letter-template.tex"
+    echo "watching $1 for changes"
+    inotifywait -qre close_write "$1"
+done
